1cdc306d5fa2cc053a8cf78f4cea999cf8d6ed12
Brad <brad@brad.tw>
2016-10-13 20:42:09 +0800

    v2

--- app/src/main/java/brad/tw/myservicetest/MainActivity.java
+++ app/src/main/java/brad/tw/myservicetest/MainActivity.java
@@ -1,7 +1,9 @@
 package brad.tw.myservicetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startService(View v){
+        Intent it = new Intent(this, MyService.class);
+        startService(it);
+    }
+    public void stopService(View v){
+        Intent it = new Intent(this, MyService.class);
+        stopService(it);
+    }
+
 }
--- app/src/main/java/brad/tw/myservicetest/MyService.java
+++ app/src/main/java/brad/tw/myservicetest/MyService.java
@@ -3,6 +3,7 @@ package brad.tw.myservicetest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
@@ -11,21 +12,25 @@ public class MyService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.v("brad", "onCreate");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.v("brad", "onDestroy");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.v("brad", "onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 }
