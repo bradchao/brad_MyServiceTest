09f7d2c923e343806668a7571e5f1215e5557eb3
Brad <brad@brad.tw>
2016-10-15 12:40:36 +0800

    v6

--- app/src/main/java/brad/tw/myservicetest/MainActivity.java
+++ app/src/main/java/brad/tw/myservicetest/MainActivity.java
@@ -1,16 +1,57 @@
 package brad.tw.myservicetest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private MyReceiver receiver;
+    private SeekBar seekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekbar = (SeekBar)findViewById(R.id.seekbar);
+
+        IntentFilter filter = new IntentFilter("brad");
+        receiver = new MyReceiver();
+        registerReceiver(receiver,filter);
+
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar,
+                                          int progress,
+                                          boolean fromUser) {
+                if (fromUser){
+                    Intent it = new Intent(MainActivity.this,
+                            MyService.class);
+                    it.putExtra("chPos", progress);
+                    startService(it);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 
     public void startService(View v){
@@ -26,5 +67,23 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(this, MyService.class);
         stopService(it);
     }
+
+
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int len = intent.getIntExtra("len",-1);
+            int nowpos = intent.getIntExtra("nowpos",-1);
+
+            if (len != -1) {
+                seekbar.setMax(len);
+            }
+            if (nowpos != -1){
+                seekbar.setProgress(nowpos);
+            }
+
+        }
+    }
+
 
 }
--- app/src/main/java/brad/tw/myservicetest/MyService.java
+++ app/src/main/java/brad/tw/myservicetest/MyService.java
@@ -6,8 +6,12 @@ import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyService extends Service {
     private MediaPlayer mp;
+    private Timer timer;
 
     public MyService() {
     }
@@ -23,19 +27,48 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         mp = MediaPlayer.create(this,R.raw.brad);
+
+        int len = mp.getDuration();
+
+        Intent it = new Intent("brad");
+        it.putExtra("len", len);
+        sendBroadcast(it);
+
+
+        timer = new Timer();
+        timer.schedule(new MyTask(),0,100);
+    }
+
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            if (mp!=null && mp.isPlaying()) {
+                int nowpos = mp.getCurrentPosition();
+                Intent it = new Intent("brad");
+                it.putExtra("nowpos", nowpos);
+                sendBroadcast(it);
+            }
+        }
     }
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         boolean isPause = intent.getBooleanExtra("isPause", false);
-        if (isPause){
-            if (mp.isPlaying()) {
-                mp.pause();
-            }
+        int chPos = intent.getIntExtra("chPos", -1);
+
+        if (chPos != -1){
+            mp.seekTo(chPos);
         }else {
-            if (!mp.isPlaying()) {
-                mp.start();
+
+            if (isPause) {
+                if (mp.isPlaying()) {
+                    mp.pause();
+                }
+            } else {
+                if (!mp.isPlaying()) {
+                    mp.start();
+                }
             }
         }
         return super.onStartCommand(intent, flags, startId);
@@ -48,6 +81,11 @@ public class MyService extends Service {
             mp.stop();
             mp.release();
             mp = null;
+        }
+        if (timer != null){
+            timer.purge();
+            timer.cancel();
+            timer = null;
         }
     }
 
